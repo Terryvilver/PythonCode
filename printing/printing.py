309c1eb2d4dcca665390d75b5ffccce435e0238a
Terry <terryvilver@gmail.com>
2019-09-22 20:41:17 +0200

    Premier programme Python

---
+++ printing/printing.py
@@ -0,0 +1,9 @@
+#Mon premier commentaire.
+print('Mon premier programme python !')
+print('*' * 70)
+print("C'est toujours un plaisir de coder sur mon nouveau langage de programmation.")
+ 
+nombre = 12
+
+print(nombre)
+
