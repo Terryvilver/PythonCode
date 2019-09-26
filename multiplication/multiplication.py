95a3afc05b2b4cb7fbba4c2ed1df6a2b179670d5
Terry <terryvilver@gmail.com>
2019-09-26 20:03:00 +0100

    exo multiplication

---
+++ multiplication/multiplication.py
@@ -0,0 +1,8 @@
+#  Écrire un programme qui affiche le résultat de la multiplication 
+#  d’un entier et d’un réel saisis préalablement au clavier.
+
+a = int(input("Saisir le premier entier : "))
+b = int(input("Saisir le second entier : "))
+c = a*b
+
+print(f'{a} x {b} = {c}')
