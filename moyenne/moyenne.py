2cbc90eb160819328b767f3674e6fba0efb96f21
Terry <terryvilver@gmail.com>
2019-09-27 23:58:52 +0200

    ajout moyenne

---
+++ moyenne/moyenne.py
@@ -0,0 +1,18 @@
+# Écrire un programme qui affiche la moyenne 
+# pondérée de trois nombres réels saisis au clavier
+# traiterez les deux cas suivants :
+# (a) les pondérations respectives sont fixées dans le programme ;
+# (b) les pondérations respectives sont saisies dans le programme.
+
+a = 5
+b = 15
+c = 12
+
+print("la moyenne est de " + str('%.1f' % ((a+b+c)/3) ) )
+
+a = input("Entrer le nombre numero 1 : ")
+b = input("Entrer le nombre numero 2 : ")
+c = input("Entrer le nombre numero 3 : ")
+
+print("la moyenne est de " + str('%.1f' % ((a+b+c)/3) ) )
+
